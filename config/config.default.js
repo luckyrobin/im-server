048b9f2e6311d25056cb789af6ae1b3be5a484af
xiongjin <you@example.com>
2020-03-24 19:16:12 +0800

    test

--- config/config.default.js
+++ config/config.default.js
@@ -30,7 +30,7 @@ module.exports = appInfo => {
       client: {
         port: 6379,          // Redis port
         host: '127.0.0.1',   // Redis host
-        // password: 'auth',
+        password: 'auth',
         db: 0,
       },
     },
