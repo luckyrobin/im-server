9031cca9f01e509b1075ce01a562d76b6a76d960
xiongjin <xiongjin@itcast.cn>
2020-04-07 12:44:57 +0800

    fix: bugs

--- app/controller/addressBook.js
+++ app/controller/addressBook.js
@@ -7,7 +7,7 @@ class AddressController extends HttpController {
     async create() {
         const { ctx, service } = this;
         const body = ctx.request.body;
-
+        console.log(body)
         try{
             let res;
             if(body.parent) {
@@ -35,9 +35,7 @@ class AddressController extends HttpController {
     async index() {
         const { ctx } = this;
         console.log('xxxxxxxx')
-        const res = await ctx.model.AddressBook.findOne({
-            name: '武汉'
-        });
+        const res = await ctx.model.AddressBook.find({});
 
         this.success({
             data: res
