f1e07c787fa602684b446afbdcbee0dee26f974e
luckyrobin <icestear@gmail.com>
2020-08-09 22:33:54 +0800

    chore: docker keyfile

--- deploy-and-start.sh
+++ deploy-and-start.sh
@@ -1,3 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
+# 生成 keyfile
+mkdir $PWD/docker-config/keyfile
+openssl rand -base64 745 > $PWD/docker-config/keyfile/mongoReplSet-keyfile
+chmod 600 $PWD/docker-config/keyfile/mongoReplSet-keyfile
+
+# 启动 Docker
 docker-compose up -d
