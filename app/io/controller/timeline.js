451f6965a7e7fd64e153ceac855a31a29a906c07
luckyrobin <icestear@gmail.com>
2021-01-22 16:06:04 +0800

    fix: 新建群组后直接保存至通讯录导致的失败的 bug

--- app/io/controller/timeline.js
+++ app/io/controller/timeline.js
@@ -21,7 +21,7 @@ class TimelineController extends HttpController {
   }
 
   async update() {
-    const { params, request } = this.ctx;
+    const { params, request, service, helper } = this.ctx;
     const body = request.body;
 
     const updatedParams = {};
@@ -30,6 +30,19 @@ class TimelineController extends HttpController {
     Reflect.has(body, 'saved') && (updatedParams.saved = body.saved);
 
     try {
+      // 如果没有创建群会话框，则自动创建一个
+      if (Reflect.has(updatedParams, 'saved')) {
+        const hasCreated = await service.io.timeline.findById(params.id);
+        if (!hasCreated) {
+          const conversation = helper.parseTimelineId(params.id);
+          await service.io.timeline.createBatch({
+            to: conversation.to,
+            from: conversation.from,
+            typeu: 2,
+          });
+        }
+      }
+
       const resp = await this.service.io.timeline.updateOneById({
         ...{ _id: params.id },
         ...updatedParams,
