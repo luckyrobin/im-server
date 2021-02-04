a43b7402037309a1438f02853d5ac36b9a36ccf7
luckyrobin <icestear@gmail.com>
2021-02-04 17:21:09 +0800

    chore: restore.sh path update

--- docker-config/backup.sh
+++ docker-config/backup.sh
@@ -7,5 +7,5 @@ FILENAME=im.20210129.archive
 mongodump -h 127.0.0.1 --port 27017 -d im --archive=/home/$FILENAME --authenticationDatabase admin -u=root -p=123456
 exit
 EOF
-docker cp $CONTAINER:/home/$FILENAME ./data/backups
+docker cp $CONTAINER:/home/$FILENAME ../data/backups
 echo 'mongodb im backup end'
--- docker-config/restore.sh
+++ docker-config/restore.sh
@@ -2,7 +2,7 @@
 echo 'mongodb im restore start...'
 CONTAINER=im-db
 FILENAME=im.20210129.archive
-docker cp ./data/backups/$FILENAME $CONTAINER:/home/
+docker cp ../data/backups/$FILENAME $CONTAINER:/home/
 docker exec -i $CONTAINER bash << 'EOF'
 FILENAME=im.20210129.archive
 mongorestore -h 127.0.0.1 --port 27017 -d im --archive=/home/$FILENAME --drop --authenticationDatabase admin -u=root -p=123456
